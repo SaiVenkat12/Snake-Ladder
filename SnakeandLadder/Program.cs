864bbaab26671b8b4881e6d2ea9748dcf261962e
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-05 19:29:36 +0530

    [SaiVenkat]-Rolling the die

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -7,7 +7,9 @@ namespace SnakeandLadder
         {
             int start = 0;
             Console.WriteLine("Snake and Ladder Game");
-            Console.WriteLine("Player 1 position is " + start);
+            var random = new Random();
+            int position = random.Next(1, 7);
+            Console.WriteLine("Player 1 position is "+position );
 
 
         }
