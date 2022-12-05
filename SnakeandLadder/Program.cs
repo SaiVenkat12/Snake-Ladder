6e69d578eb9a4e9dde0b2bab78a11a1a56376be1
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-05 23:19:59 +0530

    [SaiVenkat]-Checking What player got in the game

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -1,17 +1,46 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Xml.Linq;
+
 namespace SnakeandLadder
 {
-    internal class program
+    public class program
     {
+        public const int ladder = 0;
+        public const int a = 1;
         static void Main(string[] args)
         {
-            int start = 0;
+            int place = 0;
             Console.WriteLine("Snake and Ladder Game");
             var random = new Random();
             int position = random.Next(1, 7);
-            Console.WriteLine("Player 1 position is "+position );
+            var rand = new Random();
+            int die = rand.Next(0, 3);
+            switch (die)
+            {
+                case 0:
+                    place += position;
+                    Console.WriteLine("The player climbed the ladder");
+                    break;
+                case 1:
+                    place -= die;
+                    if (place < 0)
+                    {
+                        Console.WriteLine("The player bit by snake");
+                        place = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The player bit by snake");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("No play");
+                    break;
 
-
+            }
+            Console.WriteLine("The position of player1 is " + place);
         }
     }
 }
+
