562efc3727d3fca8561c21c64936f86b8f28b30a
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-05 19:18:43 +0530

    [SaiVenkat]-Snake and Ladder Problem

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SnakeandLadder
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            int start = 0;
+            Console.WriteLine("Snake and Ladder Game");
+            Console.WriteLine("The position of player 1 is " + start);
+
+
+        }
+    }
+}
