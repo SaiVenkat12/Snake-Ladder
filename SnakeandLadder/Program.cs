1194af7817cf9494f35efe0339956badf3e0ed83
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-05 23:50:23 +0530

    [SaiVenkat]-Player get excat 100

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -11,43 +11,41 @@ namespace SnakeandLadder
         static void Main(string[] args)
         {
             int place = 0;
+
             Console.WriteLine("Snake and Ladder Game");
             var random = new Random();
             int position = random.Next(1, 7);
             var rand = new Random();
             int die = rand.Next(0, 3);
-            while(place<=100)
+            while (place <= 1)
             {
                 switch (die)
                 {
-                    case 0:
+                    case 0:         //ladder
                         place += position;
-                        if(place >=100)
+                        if (place >= 100)
                         {
-                            place= 100;
+                            place = 100;
                         }
-                        
+
                         break;
-                    case 1:
+                    case 1:             //snake
                         place -= die;
                         if (place < 0)
                         {
-                            
+
                             place = 0;
                         }
-                        else
-                        {
-                            
-                        }
+
                         break;
                     default:
-                        
+                        place = place;
                         break;
-
                 }
             }
             Console.WriteLine("The position of player1 is " + place);
         }
     }
 }
+
 
